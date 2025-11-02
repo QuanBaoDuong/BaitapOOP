dd7a736c4271a0391f13e1269e542e4502a21ddc
Duc Anh <duccanhh2512@gmail.com>
2025-11-03 01:07:10 +0700

    gạch phá 2 lần

---
+++ src/game/object/ToughBrick.java
@@ -0,0 +1,34 @@
+package game.object;
+
+public class ToughBrick extends Brick {
+
+    private int hitCount = 0; // số lần bóng đập vào
+    private final int hitsToBreak = 2; // cần 2 lần mới vỡ
+    private String crackedImagePath = "/image/ToughBrick_Cracked.png";
+
+    public ToughBrick(int x, int y, int width, int height) {
+        super(x, y, width, height, "/image/ToughBrick.png", true);
+    }
+
+    @Override
+    public void takeHit() {
+        hitCount++;
+        if (hitCount == 1) {
+            // đổi hình sang gạch nứt
+            try {
+                java.awt.Image crackedImg = new javax.swing.ImageIcon(
+                        getClass().getResource(crackedImagePath)
+                ).getImage();
+                java.lang.reflect.Field field = Brick.class.getDeclaredField("brickImage");
+                field.setAccessible(true);
+                field.set(this, crackedImg);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (hitCount >= hitsToBreak) {
+            destroyed = true;
+        }
+    }
+}
